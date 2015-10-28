7bd934ee406a9ed6913a479d6a78ec911d3e0e44
flo3693 <florian.tissier2@gmail.com>
2015-10-28 16:59:50 +0100

    Récupération dtaa depuis xls et import dans sdf opérationnel

--- Projet_Hamon_Tissier_mercure/Program.cs
+++ Projet_Hamon_Tissier_mercure/Program.cs
@@ -11,15 +11,65 @@ namespace Projet_Hamon_Tissier_mercure
     {
         static void Main(string[] args)
         {
-            if (SQLDataController.connect())
-            {
-                if (SQLDataController.manageFamille("create", "pc"))
-                    Console.WriteLine("famille pc ajoutee");
-                else
-                    Console.WriteLine("pb creation famille pc");
-            }
+            if (importSqlFromExcel())
+                Console.WriteLine("importation reussie");
             else
-                Console.WriteLine("pb connection");
+                Console.WriteLine("Echec lors de l'importation");
         }
+
+
+
+        static bool importSqlFromExcel()
+        {
+            if (!ExcelDataController.connect())
+            {
+                Console.WriteLine("Probleme ouverture fichier xls");
+                return false;
+            }
+            if (!ExcelDataController.readXls())
+            {
+                Console.WriteLine("Probleme lecture fichier xls");
+                ExcelDataController.disconnect();
+                return false;
+            }
+            DataSet data = ExcelDataController.DataSet;
+            ExcelDataController.disconnect();
+
+            if (!SQLDataController.connect())
+            {
+                Console.WriteLine("Probleme connexion bdd");
+                return false;
+            }
+
+            foreach (DataRow row in data.Tables[0].Rows)
+            {
+                int idFamille = SQLDataController.createFamille(row[3].ToString());
+                int idMarque = SQLDataController.createMarque(row[2].ToString());
+                if (idFamille == 0)
+                {
+                    Console.WriteLine("Probleme creation famille bdd");
+                    SQLDataController.disconnect();
+                    return false;
+                }
+                int idSsFamille = SQLDataController.createSousFamille(row[4].ToString(), idFamille);
+                if (idSsFamille == 0 || idMarque == 0)
+                {
+                    Console.WriteLine("Probleme creation marque ou sousFamille bdd");
+                    SQLDataController.disconnect();
+                    return false;
+                }
+                int idArticle = SQLDataController.createArticle(idMarque, row[1].ToString(), row[5].ToString().Replace(",", "."), row[0].ToString(), idSsFamille);
+                if (idArticle == 0)
+                {
+                    Console.WriteLine("Probleme creation article bdd");
+                    SQLDataController.disconnect();
+                    return false;
+                }
+            }
+
+            SQLDataController.disconnect();
+            return true;
+        }
+
     }
 }
--- Projet_Hamon_Tissier_mercure/SQLDataController.cs
+++ Projet_Hamon_Tissier_mercure/SQLDataController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Common;
 using System.Data.SqlClient;
+using System.Data.SqlServerCe;
 using System.Data;
 using System.IO;
 using System.Linq;
@@ -16,19 +17,21 @@ namespace Projet_Hamon_Tissier_mercure
         private const string conf = "Jet2.txt";
         private const string catSql = "sql";
         private const string dataTitle = "Data Source";
+        private const string provTitle = "Provider";
         private const string defaut = "";
-        private static SqlConnection conn;
-        private static SqlCommand cmd;
+        private static SqlCeConnection conn;
+        private static SqlCeCommand cmd;
 
         public static bool connect()
         {
+            string provider = ConfigIni.GetString(conf, catSql, provTitle, defaut);
             string dataSource = ConfigIni.GetString(conf, catSql, dataTitle, defaut);
             //DbProviderFactory dbpf = DbProviderFactories.GetFactory(provider);
-            conn = new SqlConnection();
+            conn = new SqlCeConnection();
              try
             {
-            conn.ConnectionString = dataTitle + "=" + dataSource +";";
-            conn.Open();
+                conn.ConnectionString = dataTitle + "=" + dataSource +";";
+                conn.Open();
             }
             catch (Exception e)
             {
@@ -44,97 +47,100 @@ namespace Projet_Hamon_Tissier_mercure
             conn.Close();
         }
 
-        public static bool manageFamille(String type,String nomFamille, int idFamille=0)
+        public static int createFamille(String nomFamille)
         {
-            switch (type){
-                case "create":
-                    if (exists("Famille", nomFamille, "nom_famille"))
-                        return false;
-                    cmd = new SqlCommand("Insert into Famille(nom_famille) Value("+nomFamille+")", conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                case "update":
-                    if (!exists("Famille", idFamille.ToString(), "id_famille"))
-                        return false;
-                    cmd = new SqlCommand("Update Famille Set nom_famille =" + nomFamille + " WHERE id_famille="+idFamille, conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                case "delete":
-                    if (!exists("Famille", idFamille.ToString(), "id_famille"))
-                        return false;
-                    cmd = new SqlCommand("Delete from Famille where id_famille = " + idFamille, conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                default:
-                    return false;
-            }
-        }
-
-        public static bool manageSousFamille(String type, String nomSsFamille, int idSsFamille = 0, int idFamille = 0)
-        {
-            switch (type)
+            cmd = new SqlCeCommand("Select id_famille from Famille where nom_famille ='"+nomFamille+"'", conn);
+            var res = cmd.ExecuteScalar();
+            if(res != null)
+                return (int)res;
+            else
             {
-                case "create":
-                    if (exists("SousFamille", nomSsFamille, "nom_ssfamille"))
-                        return false;
-                    cmd = new SqlCommand("Insert into SousFamille(nom_ssfamille, id_famille) Value(" + nomSsFamille + ","+ idFamille +")", conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                case "updateNom":
-                    if (!exists("SousFamille", idSsFamille.ToString(), "id_ssfamille"))
-                        return false;
-                    cmd = new SqlCommand("Update SousFamille Set nom_ssfamille =" + nomSsFamille + " WHERE id_ssfamille=" + idSsFamille, conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                case "updateFamille":
-                    if (!exists("SousFamille", idSsFamille.ToString(), "id_ssfamille"))
-                        return false;
-                    cmd = new SqlCommand("Update SousFamille Set id_famille =" + idFamille + " WHERE id_ssfamille=" + idSsFamille, conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                case "delete":
-                    if (!exists("SousFamille", idSsFamille.ToString(), "id_ssfamille"))
-                        return false;
-                    cmd = new SqlCommand("Delete from SousFamille where id_ssfamille = " + idSsFamille, conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                default:
-                    return false;
+                cmd = new SqlCeCommand("Insert into Famille(nom_famille) Values('" + nomFamille + "')", conn);
+                if(!cmd.ExecuteNonQuery().Equals(1))
+                    return 0;
+                cmd = new SqlCeCommand("Select max(id_famille) From Famille", conn);
+                var res2 = cmd.ExecuteScalar();
+                if (res2 != null)
+                    return (int)res2;
+                else
+                    return 0;
             }
         }
 
-        public static bool manageArticle(String type, String nomArticle, int idArticle = 0, int idMarque = 0)
+        public static int createSousFamille(String nomSsFamille, int idFamille)
         {
-            switch (type)
+            cmd = new SqlCeCommand("Select id_ssFamille from SousFamille where nom_ssFamille ='" + nomSsFamille + "'", conn);
+            var res = cmd.ExecuteScalar();
+            if (res != null)
+                return (int)res;
+            else
             {
-                case "create":
-                    if (exists("Article", nomArticle, "nom_article"))
-                        return false;
-                    cmd = new SqlCommand("Insert into Article(nom_article, id_marque) Value(" + nomArticle + "," + idMarque + ")", conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                case "updateNom":
-                    if (!exists("Article", idArticle.ToString(), "id_article"))
-                        return false;
-                    cmd = new SqlCommand("Update Article Set nom_article =" + nomArticle + " WHERE id_article=" + idArticle, conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                case "updateMarque":
-                    if (!exists("Article", idArticle.ToString(), "id_article"))
-                        return false;
-                    cmd = new SqlCommand("Update Article Set id_marque =" + idMarque + " WHERE id_article=" + idArticle, conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                case "delete":
-                    if (!exists("Article", idArticle.ToString(), "id_article"))
-                        return false;
-                    cmd = new SqlCommand("Delete from Article where id_article = " + idArticle, conn);
-                    return cmd.ExecuteNonQuery().Equals(1);
-                default:
-                    return false;
+                cmd = new SqlCeCommand("Insert into SousFamille(nom_ssFamille, id_famille) Values('" + nomSsFamille + "'," + idFamille + ")", conn);
+                if (!cmd.ExecuteNonQuery().Equals(1))
+                    return 0;
+                cmd = new SqlCeCommand("Select max(id_ssFamille) From SousFamille", conn);
+                var res2 = cmd.ExecuteScalar();
+                if (res2 != null)
+                    return (int)res2;
+                else
+                    return 0;
             }
         }
 
-        public static bool manageMarque(){
-        
-            return false;
+        public static int createArticle(int idMarque, String refArticle, string prixHT, string description, int idSsFamille)
+        {
+
+            cmd = new SqlCeCommand("Select id_article from Article where ref_article ='" + refArticle + "'", conn);
+            var res = cmd.ExecuteScalar();
+            if (res != null)
+                return (int)res;
+            else
+            {
+                cmd = new SqlCeCommand("Insert into Article(ref_article, prixHT, description, id_marque, id_ssFamille) "
+                                + " Values('" + refArticle + "'," + prixHT + ",'" + description + "'," + idMarque + "," + idSsFamille + ")", conn);
+                if (!cmd.ExecuteNonQuery().Equals(1))
+                    return 0;
+                cmd = new SqlCeCommand("Select max(id_article) From Article", conn);
+                var res2 = cmd.ExecuteScalar();
+                if (res2 != null)
+                    return (int)res2;
+                else
+                    return 0;
+            }
         }
 
-        public static bool exists(String table, String valeur, String colonne){
-            cmd = new SqlCommand("Select count(*) from " + table + " WHERE "+colonne+"="+valeur, conn);
-            
+        public static int createMarque(String nomMarque)
+        {
+            cmd = new SqlCeCommand("Select id_marque from Marque where nom_marque ='" + nomMarque + "'", conn);
+            var res = cmd.ExecuteScalar();
+            if (res != null)
+                return (int)res;
+            else
+            {
+                cmd = new SqlCeCommand("Insert into Marque(nom_marque) Values('" + nomMarque + "')", conn);
+                if (!cmd.ExecuteNonQuery().Equals(1))
+                    return 0;
+                cmd = new SqlCeCommand("Select max(id_marque) From Marque", conn);
+                var res2 = cmd.ExecuteScalar();
+                if (res2 != null)
+                    return (int)res2;
+                else
+                    return 0;
+            }
+        }
+
+        public static bool exists(String table, String valeur, String colonne)
+        {
+            cmd = new SqlCeCommand("Select count(*) from " + table + " WHERE " + colonne + "='" + valeur+"'", conn);
             return cmd.ExecuteScalar().Equals(1);
         }
+
+        public static bool exists(String table, double valeur, String colonne)
+        {
+            cmd = new SqlCeCommand("Select count(*) from " + table + " WHERE " + colonne + "=" + valeur , conn);
+            return cmd.ExecuteScalar().Equals(1);
+        }
+
 
     }
 }
